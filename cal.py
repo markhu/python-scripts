c9a504679c7b214d0bc82bfd74d938965f1ac29a
Mark <mark.github@mhudson.net>
2014-04-19 13:38:15 -0700

    simplistic working cal

---
+++ cal.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# cal.py
+# emulate the original Unix `cal` command
+
+import sys
+
+import calendar
+import time   # simpler than datetime
+
+calendar.setfirstweekday(calendar.SUNDAY)
+
+if len(sys.argv) == 2:
+  Y= sys.argv[1]
+  calendar.prcal( int(Y) )
+elif len(sys.argv) == 3:
+  Y= sys.argv[1]
+  m= sys.argv[2]
+  calendar.prmonth( int(Y), int(m) )
+else:
+  Y= time.strftime('%Y')
+  m= time.strftime('%m')
+  calendar.prmonth( int(Y), int(m) )
+
+#EOF
