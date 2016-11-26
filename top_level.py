80804de2b00e8e29f3e616f7dc7817da081022e2
Mark Hudson <mark.github@mhudson.net>
2016-11-25 19:57:37 -0700

    link to stackoverflow.com about import

--- top_level.py
+++ top_level.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+# http://stackoverflow.com/questions/1260792/python-import-a-file-from-a-subdirectory
 # libs/__init__.py  # file must exist, even if empty
 # from libs import lower_level
 import libs.lower_level as lower_level  # otherwise default: libs.lower_level
