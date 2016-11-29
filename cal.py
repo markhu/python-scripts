fcfd46392bef0ecacc04c37e98c6092b6767fc84
Mark Hudson <mhudson@rubiconproject.com>
2016-11-29 13:15:40 -0800

    upd. travis

--- cal.py
+++ cal.py
@@ -12,16 +12,23 @@ import time   # simpler than datetime
 
 calendar.setfirstweekday(calendar.SUNDAY)
 
-if len(sys.argv) == 2:
-    Y = sys.argv[1]
-    calendar.prcal(int(Y))
-elif len(sys.argv) == 3:
-    Y = sys.argv[1]
-    m = sys.argv[2]
-    calendar.prmonth(int(Y), int(m))
-else:
-    Y = time.strftime('%Y')
-    m = time.strftime('%m')
-    calendar.prmonth(int(Y), int(m))
+def cal():
+    if len(sys.argv) == 2:
+        Y = sys.argv[1]
+        calendar.prcal(int(Y))
+    elif len(sys.argv) == 3:
+        Y = sys.argv[1]
+        m = sys.argv[2]
+        calendar.prmonth(int(Y), int(m))
+    else:
+        Y = time.strftime('%Y')
+        m = time.strftime('%m')
+        calendar.prmonth(int(Y), int(m))
+
+def test_cal():
+    pass
+
+if __name__ == "__main__":
+    cal()
 
 # EOF
