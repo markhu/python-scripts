32647b5fbd9e7637dcf5a741a4b6b456ec3ac3ab
Mark Hudson <mhudson@rubiconproject.com>
2016-11-29 13:48:59 -0800

    fix cal.py for travis

--- cal.py
+++ cal.py
@@ -12,23 +12,30 @@ import time   # simpler than datetime
 
 calendar.setfirstweekday(calendar.SUNDAY)
 
-def cal():
-    if len(sys.argv) == 2:
-        Y = sys.argv[1]
-        calendar.prcal(int(Y))
-    elif len(sys.argv) == 3:
-        Y = sys.argv[1]
-        m = sys.argv[2]
-        calendar.prmonth(int(Y), int(m))
-    else:
-        Y = time.strftime('%Y')
-        m = time.strftime('%m')
-        calendar.prmonth(int(Y), int(m))
+def cal_y(Y):
+    calendar.prcal(int(Y))
+
+def cal(m, Y):
+    calendar.prmonth(int(Y), int(m))
 
 def test_cal():
+    print ""
+    Y = time.strftime('%Y')
+    m = time.strftime('%m')
+    cal(m,Y)
     pass
 
 if __name__ == "__main__":
-    cal()
+    if len(sys.argv) == 2:
+        Y = sys.argv[1]
+        cal_y(Y)
+    elif len(sys.argv) == 3:
+        m = sys.argv[1]
+        Y = sys.argv[2]
+        cal(m,Y)
+    else:
+        Y = time.strftime('%Y')
+        m = time.strftime('%m')
+        cal(m,Y)
 
 # EOF
